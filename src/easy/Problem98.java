32d5109f8d8d2ec9e039cbac52e823c7c3279a50
Joan Sanchez <jsanchezgarcia13@gmail.com>
2022-10-04 14:36:23 +0200

    Problem 98 working

--- src/easy/Problem98.java
+++ src/easy/Problem98.java
@@ -3,7 +3,7 @@ package easy;
 import java.util.Stack;
 
 public class Problem98 {
-    public boolean isValidBST(TreeNode root) {
+    private boolean isValidBSTNonWorking(TreeNode root) {
         Stack<TreeNode> dfs = new Stack<TreeNode>();
         dfs.push(root);
         return recursive(dfs);
@@ -31,6 +31,33 @@ public class Problem98 {
             }
         }
         return rightRecursive && leftRecursive;
+    }
+
+    public boolean isValidBSTIterative(TreeNode root) {
+        if (root == null) return true;
+        Stack<TreeNode> stack = new Stack<>();
+        TreeNode pre = null;
+        while (root != null || !stack.isEmpty()) {
+            while (root != null) {
+                stack.push(root);
+                root = root.left;
+            }
+            root = stack.pop();
+            if(pre != null && root.val <= pre.val) return false;
+            pre = root;
+            root = root.right;
+        }
+        return true;
+    }
+
+    public boolean isValidBST(TreeNode root) {
+        return isValidBST(root, Long.MIN_VALUE, Long.MAX_VALUE);
+    }
+
+    public boolean isValidBST(TreeNode root, long minVal, long maxVal) {
+        if (root == null) return true;
+        if (root.val >= maxVal || root.val <= minVal) return false;
+        return isValidBST(root.left, minVal, root.val) && isValidBST(root.right, root.val, maxVal);
     }
 }
 
