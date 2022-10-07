2a5f2487314ed9526c9ef45a431d449c1eb42cb8
Joan Sanchez <jsanchezgarcia13@gmail.com>
2022-10-07 13:44:07 +0200

    Problem 438 green version

--- src/medium/Problem438/Problem438.java
+++ src/medium/Problem438/Problem438.java
@@ -1,28 +1,40 @@
 package medium.Problem438;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 public class Problem438 {
     public List<Integer> findAnagrams(String s, String p) {
         List<Integer> anagrams = new ArrayList<>();
         if (p.length() > s.length()) return anagrams;
+
+        final char[] sArray = s.toCharArray();
+        final char[] pArray = p.toCharArray();
+
+        Map<Character, Integer> sMap = new HashMap<>();
+        Map<Character, Integer> pMap = new HashMap<>();
+
+        for(int i = 0; i < p.length(); ++i){
+            Integer valuePresent = pMap.getOrDefault(pArray[i], 0);
+            pMap.put(pArray[i], ++valuePresent);
+            valuePresent = sMap.getOrDefault(sArray[i], 0);
+            sMap.put(sArray[i], ++valuePresent);
+        }
+        if(sMap.equals(pMap)){anagrams.add(0);}
+
         int firstPointer= 0;
-        for(int secondPointer = p.length()-1; secondPointer < s.length(); ++ secondPointer){
-            if(isAnagram(s.substring(firstPointer, secondPointer+1), p)){
-                anagrams.add(firstPointer);
+
+        for(int secondPointer = p.length(); secondPointer < s.length(); ++ secondPointer){
+            Integer valuePresent = sMap.getOrDefault(sArray[secondPointer], 0);
+            sMap.put(sArray[secondPointer], ++valuePresent);
+            valuePresent = sMap.getOrDefault(sArray[firstPointer], 0) - 1;
+            if (valuePresent <= 0) sMap.remove(sArray[firstPointer]);
+            else sMap.put(sArray[firstPointer], valuePresent);
+
+            if(sMap.equals(pMap)){
+                anagrams.add(firstPointer+1);
             }
             ++firstPointer;
         }
         return anagrams;
-    }
-
-    private boolean isAnagram (String s, String p){
-        final char[] string1 = s.toCharArray();
-        Arrays.sort(string1);
-        final char[] string2 = p.toCharArray();
-        Arrays.sort(string2);
-        return Arrays.equals(string1, string2);
     }
 }
