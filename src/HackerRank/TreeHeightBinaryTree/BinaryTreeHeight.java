1246f84f90ff1252dfb32122890b7f7220f9c096
Joan Sanchez <jsanchezgarcia13@gmail.com>
2022-10-15 11:27:41 +0200

    Green light

---
+++ src/HackerRank/TreeHeightBinaryTree/BinaryTreeHeight.java
@@ -0,0 +1,32 @@
+package HackerRank.TreeHeightBinaryTree;
+import java.util.*;
+import java.io.*;
+/*
+public class BinaryTreeHeight {
+
+    class Node {
+        Node left;
+        Node right;
+        int data;
+
+        Node(int data) {
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+
+    class Solution {
+
+        public static int height(Node root) {
+            return Integer.max(recursive(root.left, 0), recursive(root.right, 0));
+        }
+
+        public static int recursive(Node actual, int previousLevel){
+            if (actual == null) return previousLevel;
+            else return Integer.max(recursive(actual.left, previousLevel + 1), recursive(actual.right, previousLevel + 1));
+        }
+
+        public static Node insert(Node root, int data) {
+}
+*/
